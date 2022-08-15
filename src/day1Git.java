e7431af83c6901d8133030c0a0ceacf61deea0aa
Ahmet Guzel <gzlhmt@gmail.com>
2022-08-15 22:07:39 +0300

    v7

---
+++ src/day1Git.java
@@ -0,0 +1,9 @@
+public class day1Git {
+    public static void main(String[] args) {
+
+        System.out.println("Hello  git 1. Deneme");
+        System.out.println("Hello  git 2. Deneme");
+        System.out.println("Hello  git 3. Deneme");
+        System.out.println("Hello  git 4. Deneme");
+    }
+}
